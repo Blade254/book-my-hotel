710de095779df38fca41193ddf7d646447249ae6
GauthamPughaz <gauthamleo55@gmail.com>
2017-08-24 21:57:38 +0530

    Summary completed

--- bookings/urls.py
+++ bookings/urls.py
@@ -2,4 +2,5 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [url(r'^$', views.booking, name='booking'),
-               url(r'^search/$', views.find_hotel, name='search')]
+               url(r'^search/$', views.find_hotel, name='search'),
+               url(r'^summary$', views.summary, name='summary')]
--- bookings/views.py
+++ bookings/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse
+from django.template import RequestContext
 from django.db.models import Min
 from bookings.models import BookingDetails, RoomPriceDetails, RoomDetails, DiscountDetails, HotelDetails
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponseRedirect
 import datetime
 
 
@@ -10,33 +11,34 @@ def booking(request):
         return redirect('home')
     if request.is_ajax():
         request_no = request.GET.get('request_no')
-        if request_no == 1:
+        if int(request_no) == 1:
             room_type = request.GET.get('room_type')
             room_price = RoomPriceDetails.objects.get(room_type=room_type).price_per_day
-            rooms_available = len(RoomDetails.objects.filter(room_type=room_type).filter(room_status='V'))
+            print(room_price)
+            rooms_available = len(RoomDetails.objects.filter(room_id=room_type).filter(room_status='V'))
             return JsonResponse({'room_price': room_price, 'rooms_available': rooms_available})
-        elif request_no == 2:
+        elif int(request_no) == 2:
             no_of_days = request.GET.get('no_of_days')
             records = DiscountDetails.objects.all()
             for record in records:
                 low, high = map(int, record.length_of_stay.split(':'))
-                if high.strip() != -1:
-                    if low < no_of_days <= high:
+                if str(high).strip() != -1:
+                    if low < int(no_of_days) <= high:
                         return JsonResponse({'discount_id': record.discount_id, 'offer_percent': record.offer_percent,
                                              'message': 'success'})
-                elif high.strip() == -1:
-                    if no_of_days > low:
+                elif str(high).strip() == -1:
+                    if int(no_of_days) > low:
                         return JsonResponse({'discount_id': record.discount_id, 'offer_percent': record.offer_percent,
                                              'message': 'success'})
-            return JsonResponse({'discount_id': 'OOps... No discount available'})
+            return JsonResponse({'message': 'none', 'discount_id': 'OOps... No discount available'})
     if request.method == 'POST':
         new_booking = BookingDetails()
         new_booking.guest_id = request.session['user_id']
         new_booking.booking_status = "P"
         new_booking.check_in_date = request.POST.get('check_in_date')
-        new_booking.check_in_time = request.POST.get('check_in_time')
+        new_booking.check_in_time = request.POST.get('check_in_time')+request.POST.get('session1')
         new_booking.check_out_date = request.POST.get('check_out_date')
-        new_booking.check_out_time = request.POST.get('check_out_time')
+        new_booking.check_out_time = request.POST.get('check_out_time')++request.POST.get('session2')
         new_booking.total_guests = request.POST.get('total_guests')
         new_booking.total_days = request.POST.get('total_days')
         new_booking.total_rooms = request.POST.get('total_rooms')
@@ -44,16 +46,19 @@ def booking(request):
         new_booking.total_cost = request.POST.get('total_cost')
         new_booking.booking_date = datetime.datetime.today().strftime('%Y/%m/%d')
         new_booking.discount_id = request.POST.get('discount_id')
-        new_booking.hotel_id = request.POST.get('hotel_id')
-        new_booking.room_id = request.POST.get('room_id')
+        complete_id = request.POST.get('hotel_id')
+        new_booking.hotel_id = int(complete_id[complete_id.index('-')+1:])
+        new_booking.room_id = request.POST.get('room_type')
         new_booking.save()
-        return render(request, 'summary.html', {'name': request.session['username'], 'new_booking': new_booking})
+        hotel = HotelDetails.objects.get(pk=new_booking.hotel_id)
+        return redirect('/summary?booking_id={}'.format(new_booking.booking_id))
     hotel_id = request.GET.get('hotel_id')
     hotel_name = HotelDetails.objects.get(pk=hotel_id).name
     records = RoomPriceDetails.objects.filter(hotel_id=hotel_id)
     room_types = [records[i].room_type for i in range(len(records))]
     return render(request, 'new_booking.html', {'name': request.session['username'], 'room_types': room_types,
-                                                'hotel_id': "bmh-"+str(hotel_id), 'hotel_name': hotel_name})
+                                                'hotel_id': "bmh-"+str(hotel_id), 'hotel_name': hotel_name},
+                  RequestContext(request))
 
 
 def find_hotel(request):
@@ -62,9 +67,19 @@ def find_hotel(request):
     if request.is_ajax():
         city = request.GET.get('city')
         hotels = HotelDetails.objects.filter(city=city)
-        price_list = []
+        hotels_list = []
         for hotel in hotels:
             min_price = RoomPriceDetails.objects.filter(hotel_id=hotel.hotel_id).aggregate(Min('price_per_day'))['price_per_day__min']
-            price_list.append(min_price)
-        return JsonResponse({'hotels': hotels, 'price_list': price_list})
+            hotels_list.append((hotel.hotel_id, hotel.name, min_price))
+        return JsonResponse({'hotels_list': hotels_list})
     return render(request, 'search.html', {'name': request.session['username']})
+
+
+def summary(request):
+    if 'username' not in request.session:
+        return redirect('login')
+    booking_id = request.GET.get('booking_id')
+    new_booking = BookingDetails.objects.get(pk=booking_id)
+    hotel = HotelDetails.objects.get(pk=new_booking.hotel_id)
+    return render(request, 'summary.html', {'name': request.session['username'], 'new_booking' :new_booking,
+                                            'hotel': hotel})
