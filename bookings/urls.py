28a93cb44d5fe4d795f2c239ac7c3ac7844d8d5f
GauthamPughaz <gauthamleo55@gmail.com>
2017-09-02 18:44:13 +0530

    Booking Completed

--- bookings/urls.py
+++ bookings/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [url(r'^$', views.booking, name='booking'),
                url(r'^search/$', views.find_hotel, name='search'),
-               url(r'^summary$', views.summary, name='summary')]
+               url(r'^summary$', views.summary, name='summary'),
+               url(r'^cancellation$', views.cancel_booking, name='cancel_booking')]
--- bookings/views.py
+++ bookings/views.py
@@ -1,22 +1,24 @@
 from django.shortcuts import render, redirect, reverse
 from django.template import RequestContext
 from django.db.models import Min
-from bookings.models import BookingDetails, RoomPriceDetails, RoomDetails, DiscountDetails, HotelDetails
-from django.http import JsonResponse, HttpResponseRedirect
+from bookings.models import BookingDetails, RoomPriceDetails, RoomDetails, DiscountDetails, HotelDetails, DiscountAvailed
+from django.http import JsonResponse
 import datetime
 
 
 def booking(request):
+
     if 'username' not in request.session:
         return redirect('home')
     if request.is_ajax():
         request_no = request.GET.get('request_no')
-        room_type = request.GET.get('room_type')
         if int(request_no) == 1:
+            room_type = request.GET.get('room_type')
             room_price = RoomPriceDetails.objects.get(room_type=room_type).price_per_day
             rooms_available = len(RoomDetails.objects.filter(room_id=room_type).filter(room_status='V'))
             return JsonResponse({'room_price': room_price, 'rooms_available': rooms_available})
         elif int(request_no) == 2:
+            room_type = request.GET.get('room_type')
             no_of_days = request.GET.get('no_of_days')
             records = DiscountDetails.objects.filter(room_id=room_type)
             for record in records:
@@ -30,10 +32,11 @@ def booking(request):
                         return JsonResponse({'discount_id': record.discount_id, 'offer_percent': record.offer_percent,
                                              'message': 'success'})
             return JsonResponse({'message': 'none'})
+
     if request.method == 'POST':
         new_booking = BookingDetails()
         new_booking.guest_id = request.session['user_id']
-        new_booking.booking_status = "P"
+        new_booking.booking_status = "B"
         new_booking.check_in_date = request.POST.get('check_in_date')
         new_booking.check_in_time = request.POST.get('check_in_time')+request.POST.get('session1')
         new_booking.check_out_date = request.POST.get('check_out_date')
@@ -49,7 +52,18 @@ def booking(request):
         new_booking.hotel_id = int(complete_id[complete_id.index('-')+1:])
         new_booking.room_id = request.POST.get('room_type')
         new_booking.save()
-        return redirect(reverse('summary') + '?booking_id={}'.format(1))
+        # update room details
+        rooms = RoomDetails.objects.filter(room_id=new_booking.room_id).order_by('room_no')[:int(new_booking.total_rooms)]
+        for room in rooms:
+            room.guest_id = new_booking.guest_id
+            room.room_status = 'B'
+            room.save()
+        # update availed discount details
+        discount_availed = DiscountAvailed(discount_id=new_booking.discount_id, guest_id=new_booking.guest_id,
+                                           hotel_id=new_booking.hotel_id)
+        discount_availed.save()
+
+        return redirect(reverse('summary') + '?booking_id={}'.format(new_booking.booking_id))
     hotel_id = request.GET.get('hotel_id')
     hotel_name = HotelDetails.objects.get(pk=hotel_id).name
     records = RoomPriceDetails.objects.filter(hotel_id=hotel_id)
@@ -81,3 +95,13 @@ def summary(request):
     hotel = HotelDetails.objects.get(pk=new_booking.hotel_id)
     return render(request, 'summary.html', {'name': request.session['username'], 'new_booking' :new_booking,
                                             'hotel': hotel})
+
+
+def cancel_booking(request):
+    if 'username' not in request.session:
+        return redirect('login')
+    booking_id = request.GET.get('booking_id')
+    record = BookingDetails.objects.get(pk=booking_id)
+    record.booking_status = 'C1'
+    record.save()
+    return redirect('dashboard')
