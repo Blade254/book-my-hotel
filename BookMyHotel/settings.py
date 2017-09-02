136c316856af241eb7deb336310b98b6767ed858
GauthamPughaz <gauthamleo55@gmail.com>
2017-09-03 00:24:49 +0530

    Admin started

--- BookMyHotel/settings.py
+++ BookMyHotel/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'administration.apps.AdminConfig',
     'bookings.apps.BookingsConfig',
     'home.apps.HomeConfig',
     'django.contrib.admin',
--- BookMyHotel/urls.py
+++ BookMyHotel/urls.py
@@ -17,7 +17,8 @@ from django.conf.urls import url, include
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    url(r'^administration/', admin.site.urls),
     url(r'^', include('home.urls')),
-    url(r'^bookings/', include('bookings.urls'))
+    url(r'^bookings/', include('bookings.urls')),
+    url(r'^admin/', include('administration.urls'))
 ]
