9996de8bc5d6f4d45578a2ba152d4c8310393820
GauthamPughaz <gauthamleo55@gmail.com>
2017-09-05 00:59:07 +0530

    Employee started

--- administration/urls.py
+++ administration/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [url(r'^$', views.office_login, name='office_login'),
                url(r'^logout/$', views.office_logout, name='office_logout'),
                url(r'^create/$', views.create_employee, name='create_employee'),
                url(r'^update/$', views.update_employee, name='update_employee'),
-               url(r'^admin/$', views.admin, name='admin')]
+               url(r'^admin/$', views.admin, name='admin'),
+               url(r'^service/$', views.employee, name='employee')]
